f3f5638a6799447bb96d7796d8c633741581a403
AndriiDR <32248907+AndriiDR@users.noreply.github.com>
2017-11-18 20:15:09 -0500

    Now with candy gen

--- GameManager.cs
+++ GameManager.cs
@@ -1,16 +1,16 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
-
+	
 	public Maze mazePrefab;
-
+	
 	public Player playerPrefab;
-
+	
 	private Maze mazeInstance;
-
+	
 	private Player playerInstance;
-
+	
 	private void Start () {
 		BeginGame();
 	}
@@ -20,18 +20,19 @@ public class GameManager : MonoBehaviour {
 			RestartGame();
 		}
 	}
-
+	
 	private void BeginGame () {
 		Camera.main.clearFlags = CameraClearFlags.Skybox;
 		Camera.main.rect = new Rect(0f, 0f, 1f, 1f);
 		mazeInstance = Instantiate(mazePrefab) as Maze;
 		mazeInstance.Generate();
+        mazeInstance.GenerateCandy();
 		playerInstance = Instantiate(playerPrefab) as Player;
 		playerInstance.SetLocation(mazeInstance.GetCell(new IntVector2(0, 0)));
 		Camera.main.clearFlags = CameraClearFlags.Depth;
 		Camera.main.rect = new Rect(0f, 0f, 0.5f, 0.5f);
 	}
-
+	
 	private void RestartGame () {
 		Destroy(mazeInstance.gameObject);
 		if (playerInstance != null) {
