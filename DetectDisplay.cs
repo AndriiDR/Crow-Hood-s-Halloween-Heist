17360b0a2dc414696860568a03575c00a26b97e3
AndriiDR <32248907+AndriiDR@users.noreply.github.com>
2017-11-20 03:58:17 -0500

    Thing

--- DetectDisplay.cs
+++ DetectDisplay.cs
@@ -1,42 +1,49 @@
 using UnityEngine;
 using System.Collections;
 
-public class DetectDisplay : MonoBehaviour{
+public class DetectDisplay : MonoBehaviour
+{
 
-  public int val; //how much the percentage increases or decreases per detection
-  public Color col; //color of text
-  public int txtSize = 30; //size of text
-  
-  private int count; //detection percentage
-  private string disfield; //textbox
-  private GUIstyle myStyle = new GUIStyle();
+    public float up; //how much the percentage increases or decreases per detection
+    public float down;
+    public Color col; //color of text
+    public int txtSize = 30; //size of text
 
-  private void Start(){
-    count = 0;
-	myStyle.fontSize = txtSize;
-	myStyle.normal.textColor = col;
-  }
+    private float count; //detection percentage
+    private string disField; //textbox
+    private GUIStyle myStyle = new GUIStyle();
 
-  private void OnGUI(){
-    disField = GUI.TextField(new Rect(10, 10, 200, 20), count + "%", myStyle); 
-                             //we can add another parameter for style of the text
-    
-  }
-  
-  public void Increment(){
-    count += val;
-    OnGUI();
-  }
-  
-  public void Decrement(){
-    count -= val;
-    OnGUI();
-  }
-    
-     public float getDetection()
+    private void Start()
+    {
+        count = 0;
+        myStyle.fontSize = txtSize;
+        myStyle.normal.textColor = col;
+    }
+
+    private void OnGUI()
+    {
+        disField = GUI.TextField(new Rect(10, 10, 200, 20), count + "%", myStyle);
+        //we can add another parameter for style of the text
+
+    }
+
+    public void Increment()
+    {
+        count += up;
+        OnGUI();
+    }
+
+    public void Decrement()
+    {
+        count -= down;
+        OnGUI();
+    }
+
+    public float getDetection()
     {
         return count;
     }
 
 }
+
    
