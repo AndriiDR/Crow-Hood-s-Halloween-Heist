b62df9a3676be7dbbf9634200bc3f720616886b8
AndriiDR <32248907+AndriiDR@users.noreply.github.com>
2017-11-19 21:22:08 -0500

    Check velocity idr which direction is negative

--- Player.cs
+++ Player.cs
@@ -1,14 +1,21 @@
+using UnityEngine;
+
 public class Player : MonoBehaviour {
 	
 	private MazeCell currentCell;
 	
 	private MazeDirection currentDirection;
-	
-	public float amtForce = 1f;
 
-	public Rigidbody rb;
+    public Rigidbody rb;
 
-	public void SetLocation (MazeCell cell) {
+    private int points;
+
+    private void Start()
+    {
+        points = 0;
+    }
+
+    public void SetLocation (MazeCell cell) {
 		if (currentCell != null) {
 			currentCell.OnPlayerExited();
 		}
@@ -17,42 +24,63 @@ public class Player : MonoBehaviour {
 		currentCell.OnPlayerEntered();
 	}
 	
-	private void Move (MazeDirection direction) {
+	/*private void Move (MazeDirection direction) {
 		MazeCellEdge edge = currentCell.GetEdge(direction);
 		if (edge is MazePassage) {
 			SetLocation(edge.otherCell);
 		}
-	}
+	}*/
 	
 	private void Look (MazeDirection direction) {
 		transform.localRotation = direction.ToRotation();
 		currentDirection = direction;
 	}
-
-	private void Start() {
-		rb = GetComponent<Rigidbody>();
-	}
 	
 	private void Update () {
 		if (Input.GetKey (KeyCode.W)) {
-			rb.AddRelativeForce(Vector3.forward * amtForce * Time.deltaTime, ForceMode.Impulse);
+            rb.AddRelativeForce(Vector3.forward * 30f * Time.deltaTime, ForceMode.Impulse);
 			//transform.Translate (Vector3.forward * 5f * Time.deltaTime);
+            if (rb.velocity.x > 2f)
+            {
+                rb.velocity.x.Equals(2f);
+            }
 		}
 		if (Input.GetKey (KeyCode.S)) {
-			rb.AddRelativeForce(Vector3.back * amtForce * Time.deltaTime, ForceMode.Impulse);
-			//transform.Translate (Vector3.back * 5f * Time.deltaTime);
-		}
+            rb.AddRelativeForce(Vector3.back * 30f * Time.deltaTime, ForceMode.Impulse);
+            //transform.Translate (Vector3.back * 5f * Time.deltaTime);
+            if (rb.velocity.x < -2f)
+            {
+                rb.velocity.x.Equals(-2f);
+            }
+        }
 		if (Input.GetKey (KeyCode.A)) {
-			rb.AddRelativeForce(Vector3.left * amtForce * Time.deltaTime, ForceMode.Impulse);
-			//transform.Translate (Vector3.left * 5f * Time.deltaTime);
-		}
+            rb.AddRelativeForce(Vector3.left * 30f * Time.deltaTime, ForceMode.Impulse);
+            //transform.Translate (Vector3.left * 5f * Time.deltaTime);
+            if (rb.velocity.z > 2f)
+            {
+                rb.velocity.z.Equals(2f);
+            }
+        }
 		if (Input.GetKey (KeyCode.D)) {
-			rb.AddRelativeForce(Vector3.right * amtForce * Time.deltaTime, ForceMode.Impulse);
-			//transform.Translate (Vector3.right * 5f * Time.deltaTime);
-		}
-
+            rb.AddRelativeForce(Vector3.right * 30f * Time.deltaTime, ForceMode.Impulse);
+            //transform.Translate (Vector3.right * 5f * Time.deltaTime);
+            if (rb.velocity.z < -2)
+            {
+                rb.velocity.z.Equals(-2f);
+            }
+        }
+ 
 		float mouseInput = Input.GetAxis("Mouse X");
 		Vector3 lookhere = new Vector3(0,mouseInput,0);
 		transform.Rotate(lookhere);
 	}
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Candy"))
+        {
+            points++;
+            other.gameObject.SetActive(false);
+        }
+    }
 }
