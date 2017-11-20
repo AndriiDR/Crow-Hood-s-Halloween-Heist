21d694e1eee652b30c21b375bafaef879b1bac5f
AndriiDR <32248907+AndriiDR@users.noreply.github.com>
2017-11-20 01:14:58 -0500

    Added getDetection

--- DetectDisplay.cs
+++ DetectDisplay.cs
@@ -1,30 +1,39 @@
 using UnityEngine;
 using System.Collections;
 
-public class DetectDisplay : MonoBehaviour{
+public class DetectDisplay : MonoBehaviour
+{
 
-  public int val; //how much the percentage increases or decreases per detection
-  private int count; //detection percentage
-  private string disField; //textbox
+    public float val; //how much the percentage increases or decreases per detection
+    private float count; //detection percentage
+    private string disField; //textbox
 
-  private void Start(){
-    count = 0;
-  }
+    private void Start()
+    {
+        count = 0;
+    }
 
-  private void OnGUI(){
-    disField = GUI.TextField(new Rect(10, 10, 200, 20), count + "%"); 
-                             //we can add another parameter for style of the text
-    
-  }
-  
-  public void Increment(){
-    count += val;
-    OnGUI();
-  }
-  
-  public void Decrement(){
-    count -= val;
-    OnGUI();
-  }
+    private void OnGUI()
+    {
+        disField = GUI.TextField(new Rect(10, 10, 200, 20), count + "%");
+        //we can add another parameter for style of the text
 
+    }
+
+    public void Increment()
+    {
+        count += val;
+        OnGUI();
+    }
+
+    public void Decrement()
+    {
+        count -= val;
+        OnGUI();
+    }
+
+    public float getDetection()
+    {
+        return count;
+    }
 }
