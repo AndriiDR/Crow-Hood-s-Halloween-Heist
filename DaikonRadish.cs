5076957a672e43830d5b083b1862863040f9cb87
AndriiDR <32248907+AndriiDR@users.noreply.github.com>
2017-11-20 02:15:54 -0500

    DAIKON RADISH SLAYER OF GUARDS

---
+++ DaikonRadish.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaikonRadish : MonoBehaviour {
+
+    public DetectDisplay display;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Guard")) {
+            display.Decrement();
+            other.gameObject.SetActive(false);
+        }
+    }
+}
