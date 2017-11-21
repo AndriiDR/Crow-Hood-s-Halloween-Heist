a24ea0d8a35c6c90e78ff204e5d5ed44bd82fbf8
salongaale <salongaale@gmail.com>
2017-11-20 23:31:16 -0500

    Add files via upload

---
+++ CallEnd.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallEnd : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		if (winCondition () == true) {
+			Application.LoadLevel ("EndGame");
+		}
+	}
+
+	public bool winCondition(){
+		if(){
+			return true;
+	}
+}
